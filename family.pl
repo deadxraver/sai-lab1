892aa8f48d3dd6c5c04e3e02b4d26a262396406d
deadxraver <deadxraver@yandex.ru>
2025-09-05 14:17:30 +0300

    added new rules, relations and formatted

--- family.pl
+++ family.pl
@@ -9,7 +9,10 @@ married(max, nastya, 2018).
 married(regina, ali, 2020).
 
 % правила для браков
-areMarried(X, Y) :- married(X,Y,_);married(Y,X,_).
+areMarried(X, Y) :-
+  married(X, Y, _);
+  married(Y, X, _)
+  . % 1 правило
 
 % = дети и родители =
 parent(oksana, misha).
@@ -50,13 +53,48 @@ parent(vasilii, natasha).
 parent(lyubov, oksana).
 parent(lyubov, natasha).
 
+parent(raya, nina).
+
 % домашние животные
 owner(makar, barsik).
 owner(anya, cat).
 owner(max, nala).
 owner(regina, 'big cat').
 owner(lyubov, luchik).
+owner(natusik, matilda).
 
-% правила брат-сестра + 1 задано вручную
+% правила брат-сестра + 1 признак задан вручную
 siblings(vasilii, raya).
-areSiblings(X, Y) :- siblings(X, Y);siblings(Y, X). % TODO: сравнить родителей
+areSiblings(X, Y) :-
+    siblings(X, Y);
+    siblings(Y, X);
+    X \= Y,
+    setof(Parent, parent(Parent, X), SetOfParents1),
+    setof(Parent, parent(Parent, Y), SetOfParents2),
+    SetOfParents1 == SetOfParents2
+  .  % 2 правило
+
+% даты рождения и смерти
+born(danek, 2005).
+born(oksana, 1978).
+born(oleg, 1977).
+born(makar, 2012).
+born(misha, 2016).
+born(barsik, 2022).
+
+died(matilda, 2025).
+
+% правила для того чтобы быть человеком
+person(X) :-
+  \+owner(_, X),
+  (
+    parent(X, _);
+    parent(_, X);
+    siblings(X, _);
+    siblings(_, X);
+    born(X, _)
+  ).  % 3 правило
+
+% проверка живой - нет
+alive(X) :- \+died(X, _). % 4 правило
+
